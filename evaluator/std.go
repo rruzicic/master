f5a328038599b84f7d5fc68f1402edbedc0e24f7
Ratko Ruzicic <ratko.ruzicic@oaklandgrp.com>
2025-04-27 12:34:13 +0200

    feat: file read and write functions

--- evaluator/std.go
+++ evaluator/std.go
@@ -1,9 +1,11 @@
 package evaluator
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
 	"interpreter/object"
+	"os"
 )
 
 var stdFunc = map[string]*object.StdFunction{
@@ -30,6 +32,61 @@ var stdFunc = map[string]*object.StdFunction{
 			default:
 				return &object.Integer{Value: 0}
 			}
+		},
+	},
+	"panic": {
+		Fun: func(params ...object.Object) object.Object {
+			var buf bytes.Buffer
+			for _, param := range params {
+				buf.WriteString(param.Inspect())
+				buf.WriteString(" ")
+			}
+			fmt.Println(buf.String())
+			os.Exit(1)
+			return nil
+		},
+	},
+	"read": {
+		Fun: func(params ...object.Object) object.Object {
+			if len(params) != 1 {
+				return &object.Error{Error: "read function only accepts one parameter"}
+			}
+			filename, ok := params[0].(*object.String)
+			if !ok {
+				return &object.Error{Error: "filename must be a string"}
+			}
+			f, err := os.ReadFile(filename.Value)
+			if err != nil {
+				return &object.Error{Error: "could not open file: " + err.Error()}
+			}
+			return &object.String{Value: string(f)}
+		},
+	},
+	"write": {
+		Fun: func(params ...object.Object) object.Object {
+			if len(params) != 2 {
+				return &object.Error{Error: "read function only accepts two parameters"}
+			}
+			filename, ok := params[0].(*object.String)
+			if !ok {
+				return &object.Error{Error: "filename must be a string"}
+			}
+			data := params[1].(*object.String)
+			if !ok {
+				return &object.Error{Error: "data must be string"}
+			}
+			f, err := os.Create(filename.Value)
+			if err != nil {
+				return &object.Error{Error: "could not open file: " + err.Error()}
+			}
+			defer f.Close()
+			w := bufio.NewWriter(f)
+			_, err = w.WriteString(data.Value)
+			if err != nil {
+				return &object.Error{Error: "could not write to file: " + err.Error()}
+			}
+			w.Flush()
+			return nil
 		},
 	},
 }
